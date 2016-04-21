92584ce196d50bb2aabf9464e2988fe5e3662ec4
madacol <madacol10@gmail.com>
2016-04-21 03:22:08 -0430

    added has_auth_to method to Employee

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,6 +1,23 @@
 class Employee < ActiveRecord::Base
+
 	has_many :employee_groups
 	has_many :comision_odts
 	has_many :odts, through: :comision_odts
 	has_many :auth_groups, through: :employee_groups
+
+	def has_auth_to(action)
+		self.auth_groups.each do |group|
+			case group[action]
+			when true
+				return true 
+			when false
+				return false
+			when nil
+				return 'Action '+action.to_s+' not found'
+			else
+				return 'Not boolean'
+			end
+		end
+	end
+
 end
