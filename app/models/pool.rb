0f7b47db40d033a933173ff74ce852139daaaacb
yuliethc <luboyulieth@gmail.com>
2016-04-27 23:54:34 -0430

    RazonSocials added to Pool's view: show, and to Odt's view: show and form

--- app/models/pool.rb
+++ app/models/pool.rb
@@ -1,3 +1,4 @@
 class Pool < ActiveRecord::Base  
-  belongs_to :category, polymorphic: true  
+  belongs_to :category, polymorphic: true 
+  belongs_to :razon_social   
 end
--- app/models/razon_social.rb
+++ app/models/razon_social.rb
@@ -1,6 +1,7 @@
 class RazonSocial < ActiveRecord::Base
     has_many :odts
     has_many :retenciones
+    has_many :pools
 
     def table_name_to_show
     	'Razon Social'
