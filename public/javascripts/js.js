1ca8540c995a68fe20229fce6ad9dfa59a8cd227
madacol <madacol10@gmail.com>
2016-05-28 01:48:25 -0400

    Fixed modals on pools so there can be multiple instances

--- public/javascripts/js.js
+++ public/javascripts/js.js
@@ -13,19 +13,21 @@ $(document).ready(function(){
     $(this).hide()
     $(this).find('select').removeAttr('name');
   });
-  var select = $("#pool_category_type");
-  select.data( "previous",select.val() );
-  select.change( function (){
-    old_select_category_id = $('#'+$(this).data('previous'));
-    old_select_category_id.addClass('hidden');
-    old_select_category_id.hide();
-    old_select_category_id.find('select').removeAttr('name');
+  $("select[id^='pool_category_type']").each(function() {
+    var select = $(this);
+    select.data( "previous",select.val() );
+    select.change( function (){
+      old_select_category_id = $('#'+$(this).data('previous')+$(this).parents('form').attr('id') );
+      old_select_category_id.addClass('hidden');
+      old_select_category_id.hide();
+      old_select_category_id.find('select').removeAttr('name');
 
-    new_select_category_id = $('#'+$(this).val());
-    new_select_category_id.removeClass('hidden');
-    new_select_category_id.show();
-    new_select_category_id.find('select').prop('name', 'pool[category_id]');
+      new_select_category_id = $('#'+$(this).val()+$(this).parents('form').attr('id') );
+      new_select_category_id.removeClass('hidden');
+      new_select_category_id.show();
+      new_select_category_id.find('select').prop('name', 'pool[category_id]');
 
-    $(this).data("previous",$(this).val());
-  });
+      $(this).data("previous",$(this).val());
+    });
+  });    
 });
