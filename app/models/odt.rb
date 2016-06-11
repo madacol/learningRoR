e815b481eaa4e275d7270066a5bbc214892cfa6b
madacol <madacol10@gmail.com>
2016-06-11 13:43:11 -0400

    Now can add one comision directly form Odt's form

--- app/models/odt.rb
+++ app/models/odt.rb
@@ -1,10 +1,11 @@
 class Odt < ActiveRecord::Base
-    belongs_to :razon_social
-    has_many :deducciones_odts
-    has_many :comision_odts
+	belongs_to :razon_social
+	has_many :deducciones_odts
+	has_many :comision_odts, :dependent => :destroy
 	has_many :employees, through: :comision_odts
 	has_many :pools, as: :category
-	accepts_nested_attributes_for :comision_odts, allow_destroy: true, reject_if: :all_blank
+	accepts_nested_attributes_for :comision_odts, allow_destroy: true
+
 
 	def table_name_to_show
 		'Odt'
