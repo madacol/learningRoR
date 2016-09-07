b2ddcaf59a1314b02897b36c634935a3789dfcc1
yuliethc <luboyulieth@gmail.com>
2016-09-07 01:01:11 -0400

    Fixed mistakes made last merge

--- app/models/razon_social.rb
+++ app/models/razon_social.rb
@@ -1,6 +1,7 @@
 class RazonSocial < ActiveRecord::Base
     validates_presence_of :name, presence: true, :message => "no puede estar vacío"
     validates_presence_of :rif_ci, presence: true, :message => "no puede estar vacío"
+    has_many :odts
     has_many :retenciones
     has_many :bdvs
     has_many :pools
